30aa14d4b7476e93abc8032ec385d25321eca489
minji <96130585+minjiLeee@users.noreply.github.com>
2024-03-20 17:41:01 +0900

    Delete proj_woofy directory

--- proj_woofy/src/components/footer/Footer.js
+++
@@ -1,5 +0,0 @@
-const Footer = () => {
-  return <Footer>푸터</Footer>;
-};
-
-export default Footer;
--- proj_woofy/src/components/navbar/Navbar.js
+++
@@ -1,5 +0,0 @@
-const Navbar = () => {
-  return <Navbar>네비바</Navbar>;
-};
-
-export default Navbar;
