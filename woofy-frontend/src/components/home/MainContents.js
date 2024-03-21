10a8b3bfd04e72d19ae38117b133b5eeae4c508c
minjiLeee <minji5203@gmail.com>
2024-03-21 15:28:45 +0900

    test

---
+++ woofy-frontend/src/components/home/MainContents.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Logo from "../navbar/Logo";
+
+const MainContents = () => {
+  return (
+    <div>
+      <Logo />
+      <h1>Home</h1>
+      <p>이곳은 Home 입니다.</p>
+    </div>
+  );
+};
+
+export default MainContents;
